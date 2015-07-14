5b962a75be3e0ae881a144ae5f256a8217eac7f0
Asbjørn Enge <asbjorn@hanafjedle.net>
2015-07-14 10:13:47 +0200

    Included another peer in the test

--- test/spec.js
+++ test/spec.js
@@ -3,13 +3,18 @@ var mdp    = require('../index')
 
 it('can find peers', function(done) {
     var p1 = mdp('testpeers')
+    var peers = []
     p1.on('peer', function(peer) {
-        assert(peer[0].data == p2.id)
+        assert([p2.id, p3.id].indexOf(peer[0].data) >= 0)
+        peers.push(peer)
+        if (peer[0].data == p2.id) p2.destroy()
+        if (peer[0].data == p3.id) p3.destroy()
+        if (peers.length < 2) return
         p1.destroy()
-        p2.destroy()
         done()
     })
     var p2 = mdp('testpeers')
+    var p3 = mdp('testpeers')
 })
 
 it('will answer with passed answers', function(done) {
